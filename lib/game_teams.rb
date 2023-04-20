b6e955176e6dd038f6ca8530216bb28e91257ea1
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-20 16:50:02 -0600

    Feature: Created Game_teams class and Game_team_module

---
+++ lib/game_teams.rb
@@ -0,0 +1,15 @@
+class GameTeams
+    attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals, :shots, :tackles
+  def initialize(details)
+      @game_id = details[:game_id],
+      @team_id = details[:team_id],
+      @hoa = details[:hoa],
+      @result = details[:result],
+      @settled_in = details[:settled_in],
+      @head_coach = details[:head_coach],
+      @goals = details[:goals],
+      @shots = details[:shots],
+      @tackles = details[:tackles]
+      require 'pry'; binding.pry
+  end
+end
