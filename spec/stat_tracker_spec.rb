0cb8408c6573ee07b1fd7f4352c76e2e72b893cc
Andy Weissman <andyweissman6@gmail.com>
2023-04-21 09:38:47 -0600

    test: adds tests for #highest_total_score, #lowest_total_score, #percentage_home_wins, #percentage_visitor_wins

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require_relative "spec_helper"
 
 RSpec.describe StatTracker do
   describe '#initialize' do
@@ -39,6 +39,70 @@ RSpec.describe StatTracker do
       expect(stat_tracker.games[0].away_goals).to eq("2")
       expect(stat_tracker.games[1].game_id).to eq("2012030222")
       expect(stat_tracker.games[2].total_goals).to eq(3)
+    end
+  end
+
+  describe "#highest_total_score" do
+    it "returns the highest sum of the winning and losing teams’ scores" do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+      locations = 
+      {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      expect(stat_tracker.highest_total_score).to eq(11)
+    end
+  end
+
+  describe "#lowest_total_score" do
+    it "returns the lowest sum of the winning and losing teams’ scores" do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+      locations = 
+      {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      expect(stat_tracker.lowest_total_score).to eq(0)
+    end
+  end
+
+  describe "#percentage_home_wins" do
+    it "returns Percentage of games that a home team has won (rounded to the nearest 100th)" do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+      locations = 
+      {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      expect(stat_tracker.percentage_home_wins).to eq(0.44)
+    end
+  end
+
+  describe "#percentage_visitor_wins" do
+    it "returns Percentage of games that a home team has won (rounded to the nearest 100th)" do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+      locations = 
+      {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      expect(stat_tracker.percentage_visitor_wins).to eq(0.36)
     end
   end
 end
