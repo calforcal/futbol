ebdc21d3a7fd52184ceafab30ea3050ef1d27731
Andy Weissman <andyweissman6@gmail.com>
2023-04-21 09:40:44 -0600

    add #highest_total_score,  #lowest_total_score, #percentage_home_wins, #percentage_visitor_wins methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,5 @@
 require "csv"
-require './lib/team_factory_module'  
+require_relative './team_factory_module'  
 require_relative './game_factory_module'
 require_relative './game_teams_factory_module'
 
@@ -19,5 +19,35 @@ class StatTracker
   
   def self.from_csv(locations)
     StatTracker.new(locations)
+  end
+
+  def highest_total_score
+    total = @games.max_by do |game|
+      game.total_goals
+    end
+    total.total_goals
+  end
+
+  def lowest_total_score
+    lowest_total = @games.min_by do |game|
+      game.total_goals
+    end
+    lowest_total.total_goals
+  end
+
+  def percentage_home_wins
+    home_wins = @games.count do |game|
+      game.home_goals > game.away_goals
+    end 
+    percent = home_wins / games.count.to_f
+    percent.round(2)
+  end
+
+  def percentage_visitor_wins
+    away_wins = @games.count do |game|
+      game.away_goals > game.home_goals
+    end 
+    percent = away_wins / games.count.to_f
+    percent.round(2)
   end
 end
