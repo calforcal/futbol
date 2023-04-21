84c3a8d437d3dd3ad53dca6e1168ed83adc8d0fe
Jimmy Jurado <jurado140@gmail.com>
2023-04-21 13:16:47 -0400

    Feat: Fixes game team spec and slight refactor to gameteam module.

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -1,14 +1,14 @@
 class GameTeams
     attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals, :shots, :tackles
   def initialize(details)
-      @game_id = details[:game_id],
-      @team_id = details[:team_id],
-      @hoa = details[:hoa],
-      @result = details[:result],
-      @settled_in = details[:settled_in],
-      @head_coach = details[:head_coach],
-      @goals = details[:goals],
-      @shots = details[:shots],
+      @game_id = details[:game_id]
+      @team_id = details[:team_id]
+      @hoa = details[:hoa]
+      @result = details[:result]
+      @settled_in = details[:settled_in]
+      @head_coach = details[:head_coach]
+      @goals = details[:goals]
+      @shots = details[:shots]
       @tackles = details[:tackles]
   end
 end
--- lib/game_teams_factory_module.rb
+++ lib/game_teams_factory_module.rb
@@ -1,20 +1,21 @@
 require_relative './game_teams'
 module GameTeamsFactoryModule
   def create_game_teams(game_teams_path)
-    games_teams = CSV.open game_teams_path, headers: true, header_converters: :symbol
+    games_teams = CSV.read(game_teams_path, headers: true, header_converters: :symbol)
     games_teams.map do |game_team|
-      details = {
-        game_id: game_team[:game_id],
-        team_id: game_team[:team_id],
-        hoa: game_team[:hoa],
-        result: game_team[:result],
-        settled_in: game_team[:settled_in],
-        head_coach: game_team[:head_coach],
-        goals: game_team[:goals],
-        shots: game_team[:shots],
-        tackles: game_team[:tackles]
-    }
-    GameTeams.new(details)
+      GameTeams.new(game_team)
+    #   details = {
+    #     game_id: game_team[:game_id],
+    #     team_id: game_team[:team_id],
+    #     hoa: game_team[:hoa],
+    #     result: game_team[:result],
+    #     settled_in: game_team[:settled_in],
+    #     head_coach: game_team[:head_coach],
+    #     goals: game_team[:goals],
+    #     shots: game_team[:shots],
+    #     tackles: game_team[:tackles]
+    # }
+    # x = GameTeams.new(details)
     end
   end
 end
